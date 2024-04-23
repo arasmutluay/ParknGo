c5029390443fa58f8035cd7d0e84230c5563df40
arasmutluay <arasmutluayy@gmail.com>
2024-04-24 02:09:29 +0300

    update information + carpark details page

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import current_user
 from .models import User, Carpark
@@ -55,11 +57,69 @@ def carpark_list():
                            car_parks=car_parks)
 
 
+@views.route('/carpark_details', methods=['GET', 'POST'])
+def carpark_details():
+    if not current_user.is_authenticated or current_user.role != 'admin':
+        flash("Access Denied: First you need to be logged in as an Admin!", 'error')
+        return redirect(url_for('views.home'))
+
+    car_park_id = request.form.get('car_park_id')
+    car_park = Carpark.query.filter_by(id=car_park_id).first()
+
+    if request.method == 'POST':
+
+        if car_park:
+            if request.form.get('action') == 'block':
+                block_carpark(car_park.id)
+                flash("Car park blocked successfully!", 'success')
+            elif request.form.get('action') == 'unblock':
+                unblock_carpark(car_park.id)
+                flash("Car park unblocked successfully!", 'success')
+        else:
+            flash("Car park not found.", "error")
+            return redirect(url_for('views.carpark_list'))
+
+
+    return render_template("carpark_details.html", user=current_user,
+                           car_park=car_park,
+                           car_park_id=car_park_id)
+
+
+@views.route('/update_carpark', methods=['POST'])
+def update_carpark():
+    if not current_user.is_authenticated or current_user.role != 'admin':
+        flash("Access Denied: First you need to be logged in as an Admin!", 'error')
+        return redirect(url_for('views.home'))
+
+    if request.method == 'POST':
+        car_park_id = request.form.get('carpark_id')
+        car_park = Carpark.query.get(car_park_id)
+
+        if not car_park:
+            flash("Car park not found.", "error")
+            return redirect(url_for('views.carpark_list'))
+
+        car_park.name = request.form.get('name')
+        car_park.company = request.form.get('company')
+        car_park.location = request.form.get('location')
+        car_park.price = request.form.get('price')
+        car_park.max_allowed_capacity = request.form.get('max_allowed_capacity')
+        car_park.max_total_capacity = request.form.get('max_total_capacity')
+        car_park.last_action_date = datetime.now()
+
+        db.session.commit()
+
+        flash("Car park information updated successfully!", 'success')
+
+    return redirect(url_for('views.carpark_list'))
+
+
 def block_carpark(carpark_id):
     car_park = Carpark.query.get(carpark_id)
     if car_park:
         if car_park.status != "blocked":
             car_park.status = "blocked"
+            car_park.last_action_date = datetime.now()
             db.session.commit()
         else:
             flash("Car park is already blocked.", "warning")
@@ -72,6 +132,7 @@ def unblock_carpark(carpark_id):
     if car_park:
         if car_park.status != "active":
             car_park.status = "active"
+            car_park.last_action_date = datetime.now()
             db.session.commit()
         else:
             flash("Car park is already unblocked.", "warning")
